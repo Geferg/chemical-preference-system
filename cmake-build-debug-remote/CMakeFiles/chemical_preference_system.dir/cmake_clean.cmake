0322a1277e1adeb83369e439ce915b9813181212
Kristian Klette <geferg.dev@gmail.com>
2024-02-19 12:06:38 +0100

    Configure repote raspberry pi connection

---
+++ cmake-build-debug-remote/CMakeFiles/chemical_preference_system.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chemical_preference_system.dir/main.cpp.o"
+  "CMakeFiles/chemical_preference_system.dir/main.cpp.o.d"
+  "chemical_preference_system"
+  "chemical_preference_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chemical_preference_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
